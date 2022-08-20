aa0f45b6e8705ab6e5cd4abea5d971b896bc77d7
acepele <acepele@students.mchenry.edu>
2022-08-20 00:12:51 -0500

    Initial commit

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val oneByte: Byte = 1
+    println("byte demo: $oneByte")
+
+    val myShort: Short = 16
+    println("short demo: $myShort")
+
+
+    val myInt = 32
+    println("int demo: $myInt")
+
+    val myLong = 64L
+    println("long demo: $myLong")
+
+    val myFloat = 3.14F
+    println("float demo: $myFloat")
+
+    val myDouble = 32.47586895979
+    println("double demo: $myDouble")
+
+    val myBoolean = true
+    val anotherBoolean = false
+    println("boolean demo: $myBoolean")
+    println("boolean demo: $anotherBoolean")
+
+    val myChar = 'k'
+    println("char demo: $myChar")
+
+
+}
